f8652c6565e9b619d5fe31dd8744756ca6b21d77
Kajanan <kajan.m02@outlook.fr>
2023-09-14 00:10:03 +0200

    Templates
    template work in progress.

--- Templates/OSPFv2/provision.sh
+++ Templates/OSPFv2/provision.sh
@@ -5,3 +5,40 @@ DIR=`dirname $0`
 cd `dirname $0`
 
 ###############################################################################
+
+###############################################################################
+
+apt-get update
+
+DEBIAN_FRONTEND=noninteractive apt-get install net-tools nano gedit wireshark openssh-server xfce4 dnsutils -y
+
+###############################################################################
+
+echo "
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.log_martians = 1
+" >> /etc/sysctl.conf
+
+sysctl -p
+
+###############################################################################
+
+#__add GPG key
+curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null
+
+#__possible values for FRRVER: frr-6 frr-7 frr-8 frr-stable
+#__frr-stable will be the latest official stable release
+FRRVER="frr-8"
+
+echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list
+
+###############################################################################
+
+apt-get update
+
+#__update and install FRR
+DEBIAN_FRONTEND=noninteractive apt-get install frr frr-pythontools -y
+
+###############################################################################
